02e684373e9863c1a038c84cd4d48638c06a0e3c
isikveren <lauxunzi@outlook.com>
2022-03-09 12:38:45 +0800

    update

--- 1/随机温度.cpp
+++ 1/随机温度.cpp
@@ -5,22 +5,26 @@
 int main()
 {
     FILE *fp;
-    fp = fopen("温度.txt","w+");
-    int sum = 0, value = 0;
+    fp = fopen("./温度.txt", "w+");
+    //int sum = 0, value = 0;
     time_t t;
     srand((unsigned)time(&t));
     float *r = (float *)malloc(sizeof(float) * 3);
     float *p = r;
-    for(int j = 0;j<100;j++){
-        for (int i = 0; i < 3; i++)
+    for (int j = 0; j < 80; j++)
     {
-        *p = 36 + (float)(rand() % 10) / 10;
-        p++;
+        for (int i = 0; i < 3; i++)
+        {
+            *p = 36 + (float)(rand() % 10) / 10;
+            p++;
+        }
+        p = r;
+        fprintf(fp, "早：%.1f/午：%.1f/晚：%.1f/晚点名：√\n", *p, *(p + 1), *(p + 2));
+        if((j+1)%8 == 0){
+            fprintf(fp, "\n");
+        }
+        // std::cout << "早：" << *(p) << "/午：" << *(p + 1) << "/晚：" << *(p + 2) << "/晚点名：√" << std::endl;
     }
-    p = r;
-    fprintf(fp, "早：%.1f/午：%.1f/晚：%.1f/晚点名：√\n", *p,*(p+1),*(p+2));
-    //std::cout << "早：" << *(p) << "/午：" << *(p + 1) << "/晚：" << *(p + 2) << "/晚点名：√" << std::endl;
-    }
-    
+
     return 0;
 }
